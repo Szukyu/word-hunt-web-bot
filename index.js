6fdd36dc37c91a84502ff7039a276297897746e2
Ken Lin <vermillionbirdszukyu@gmail.com>
2024-09-19 10:41:08 -0400

    testing

--- index.js
+++ index.js
@@ -257,6 +257,72 @@ class Donut {
       return this.lb[pos - 3];
     }
   }
+
+  peekRight(pos) {
+    if (pos == 2 || pos == 7 || pos == 9 || pos == 11 || pos == 16 || pos == 19) {
+      return -1;
+    } else {
+      return this.lb[pos + 1];
+    }
+  }
+
+  peekLowerRight(pos) {
+    if (pos == 4 || pos == 7 || pos == 11 || pos >= 15) {
+      return -1;
+    } else if (pos == 3 || pos == 10) {
+      return this.lb[pos + 6];
+    } else {
+      return this.lb[pos + 5];
+    }
+  }
+
+  peekDown(pos) {
+    if (pos == 5 || pos == 12 || pos >= 17) {
+      return -1;
+    } else if (pos == 4 || pos == 5 || pos == 10 || pos == 11) {
+      return this.lb[pos + 5];
+    } else {
+      return this.lb[pos + 4];
+    }
+  }
+
+  peekLowerLeft(pos) {
+    if (pos == 3 || pos == 8 || pos == 13 || pos >= 17 || pos % 6 == 0) {
+      return -1;
+    } else if (pos == 4 || pos == 5 || pos == 10 || pos == 11) {
+      return this.lb[pos + 4];
+    } else {
+      return this.lb[pos + 3];
+    }
+  }
+
+  peekLeft(pos) {
+    if (pos == 0 || pos == 3 || pos == 8 || pos == 10 || pos == 12 || pos == 17) {
+      return -1;
+    } else {
+      return this.lb[pos - 1];
+    }
+  }
+
+  visitDirection(pos, dir) {
+    const directionDict = {
+      [UPLEFT]: this.peekUpperLeft.bind(this),
+      [UP]: this.peekUp.bind(this),
+      [UPRIGHT]: this.peekUpperRight.bind(this),
+      [RIGHT]: this.peekRight.bind(this),
+      [DOWNRIGHT]: this.peekLowerRight.bind(this),
+      [DOWN]: this.peekDown.bind(this),
+      [DOWNLEFT]: this.peekLowerLeft.bind(this),
+      [LEFT]: this.peekLeft.bind(this)
+    };
+
+    const visitedLetter = directionDict[dir](pos);
+    if (visitedLetter === -1 || visitedLetter.visited) {
+      return -1;
+    }
+    visitedLetter.markVisited();
+    return visitedLetter;
+  }
 }
 
 // Main
@@ -305,12 +371,12 @@ document.getElementById("submit").onclick = function() {
     const board = new Boarder(lettersObjs);
     findValidWords(board);
     printOutput();
-  } else if (letters.length == 21) {
-    for (let i = 0; i < 21; i++) {
+  } else if (letters.length == 20) {
+    for (let i = 0; i < 20; i++) {
       inputLetters.push(letters[i]);
     }
     const letterObjs = [];
-    for (let i = 0; i < 21; i++) {
+    for (let i = 0; i < 20; i++) {
       letterObjs.push(new Letter(inputLetters[i], i));
     }
     const board = new Donut(letterObjs);
