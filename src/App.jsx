a0c8a9899d0656273837d53eda76b16b5072e0eb
Tsukuyomi <vermillionbirdszukyu@gmail.com>
2025-09-09 22:00:15 -0400

    Busy with SchoolWork. Navbar Header Bring Back to Option. Github Link Not Inline D:

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState }from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar.jsx';
 import Option from './components/Option/Option.jsx';
 
 function App() {
+  const [resetKey, setResetKey] = useState(0);
+
+  const resetOption = () => {
+    setResetKey(prev => prev + 1);
+  };
+
   return (
     <div className="App">
-      <Option />
+      <Navbar onReset={resetOption} />
+      <Option key={resetKey} />
     </div>
   );
 }
