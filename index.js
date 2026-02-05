6f68f36677ecd5e876e770b25e0454c7ab2e3314
Szu <linken0323@gmail.com>
2026-02-04 19:03:57 -0500

    Input Testing

--- index.js
+++ index.js
@@ -1,3 +1,111 @@
+// Classes
+class Letter {
+  constructor(ch, index, visit = False) {
+    this.char = ch;
+    this.pos = index;
+    this.visited = visit;
+  }
+
+  copyLetter() {
+    return new Letter(this.char, this.pos, this.visited)
+  }
+
+  markVisited() {
+    this.visited = True
+  }
+}
+
+class Board {
+  constructor(lettersArr) {
+    this.lb = lettersArr;
+  }
+
+  copyBoard() {
+    const newArr = [];
+    for (let i = 0; i < this.lb.length; i++) {
+      newArr.push(this.lb[i].copyLetter());
+    }
+    return new Board(newArr);
+  }
+
+  peekUpperLeft(pos) {
+    if (pos % 4 === 0 || pos <= 3) {
+      return -1;
+    }
+    return this.lb[pos - 5];
+  }
+
+  peekUp(pos) {
+    if (pos <= 3) {
+      return -1;
+    }
+    return this.lb[pos - 4];
+  }
+
+  peekUpperRight(pos) {
+    if (pos <= 3 || pos % 4 === 3) {
+      return -1;
+    }
+    return this.lb[pos - 3];
+  }
+
+  peekRight(pos) {
+    if (pos % 4 === 3) {
+      return -1;
+    }
+    return this.lb[pos + 1];
+  }
+
+  peekLowerRight(pos) {
+    if (pos >= 12 || pos % 4 === 3) {
+      return -1;
+    }
+    return this.lb[pos + 5];
+  }
+
+  peekDown(pos) {
+    if (pos >= 12) {
+      return -1;
+    }
+    return this.lb[pos + 4];
+  }
+
+  peekLowerLeft(pos) {
+    if (pos % 4 === 0 || pos >= 12) {
+      return -1;
+    }
+    return this.lb[pos + 3];
+  }
+
+  peekLeft(pos) {
+    if (pos % 4 === 0) {
+      return -1;
+    }
+    return this.lb[pos - 1];
+  }
+
+  visitDirection(pos, dir) {
+    const directionDict = {
+      0: this.peekUpperLeft.bind(this),
+      1: this.peekUp.bind(this),
+      2: this.peekUpperRight.bind(this),
+      3: this.peekRight.bind(this),
+      4: this.peekLowerRight.bind(this),
+      5: this.peekDown.bind(this),
+      6: this.peekLowerLeft.bind(this),
+      7: this.peekLeft.bind(this)
+    };
+
+    const visitedLetter = directionDict[dir](pos);
+    if (visitedLetter === -1 || visitedLetter.visited) {
+      return -1;
+    }
+    visitedLetter.markVisited();
+    return visitedLetter;
+  }
+}
+
+// Main
 document.addEventListener('DOMContentLoaded', function() {
   fetch('words.txt')
   .then(function(response) {
@@ -8,3 +116,7 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById('file').textContent = words;
     })
 })
+
+document.getElementById("submit").onclick = function() {
+  document.getElementById("testing").textContent = document.getElementById("letters").value;
+}
