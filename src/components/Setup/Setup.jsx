6c63a0b83b41d9a36c23b3f9fbf901f8cb0a798e
Szu <linken0323@gmail.com>
2026-02-04 19:03:59 -0500

    File Structure Cleanup and Added Letter Display When Typing

---
+++ src/components/Setup/Setup.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Board from "../Boards/Board"
+import Boarder from "../Boards/Boarder";
+import Donut from "../Boards/Donut";
+import X from "../Boards/X";
+
+const Setup = () => {
+  return (
+    <div>
+      <h2> Board Selection </h2>
+    </div>
+  )
+}
+
+export default Setup;
