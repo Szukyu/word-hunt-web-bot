3821b88b5cfaae54ee40d3bb0a5bf187f6f90c11
tsukuyomi <tsukuyomi@tsukuyomis-MacBook-Air.local>
2026-03-15 21:01:10 -0400

    I need to touch grass: Converted theme to be minimal (7 colors)
    the rice is not done. still kind of looks weird. Gonna revamp amid
    adding functionality

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -2,7 +2,7 @@ import './Tile.css'
 
 const Tile = ({ value, part }) => {
   return (
-    <div className={`tile ${part ? 'part' : ''}`}>
+    <div className={`tile ${part ? 'highlight' : ''}`}>
       {value}
     </div>
   );
